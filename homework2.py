a2af6e0d9171809920375ccbe4005f61316256f2
Rtaidem <Lanrickrezon@gmail.com>
2024-07-02 12:25:56 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+Total_tasks = 12
+Hours_spent = 1.5
+Course = "Python, "
+Time_per_stuck = (Hours_spent / Total_tasks)
+print("Курс: ", Course, "всего задач: ", Total_tasks, ",", " затрачено часов: ", Hours_spent, ",", " среднее время выполнения: ", Time_per_stuck, " часа.", sep="")
